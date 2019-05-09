282fe56a241650fa6261cdcdf2f2be9647cc4ee2
Dx18 <Dx18.E@yandex.ru>
2019-05-09 14:50:52 +0400

    Set default IP

--- desktop/src/geobattle/geobattle/desktop/DesktopLauncher.java
+++ desktop/src/geobattle/geobattle/desktop/DesktopLauncher.java
@@ -43,7 +43,7 @@ public class DesktopLauncher {
             };
 
             return new ExternalAPI(
-                    new SocketServer("localhost", 12000, oSAPI),
+                    new SocketServer("78.47.182.60", 12000, oSAPI),
                     new FixedGeolocationAPI(longitude, latitude),
                     new TileRequestPool(appId, appCode, cachePath, 10),
                     oSAPI
